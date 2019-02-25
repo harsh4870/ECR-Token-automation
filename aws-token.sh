b355470aa1c10b20677412857aba98df8a2eb625
Harsh Manvar <harsh.manvar111@gmail.com>
2019-02-25 16:52:00 +0530

    Create aws-token.sh

---
+++ aws-token.sh
@@ -0,0 +1,13 @@
+ACCOUNT=123456789 #aws account number
+REGION=ap-south-1 #aws ECR region
+SECRET_NAME=${REGION}-ecr-registry #secret_name
+EMAIL=abc@xyz.com #can be anything
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+ --docker-server=https://$ACCOUNT.dkr.ecr.ap-south-1.amazonaws.com \
+ --docker-username=AWS \
+ --docker-password="${TOKEN}" \
+ --docker-email="${EMAIL}"
